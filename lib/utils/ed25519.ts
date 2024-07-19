3c9f3da2467db39ea358352e6d4c8056c62c67ce
Opliko <opliko.reg@protonmail.com>
2024-07-19 23:44:02 +0200

    fix: don't destructure exports in public API

--- lib/utils/ed25519.ts
+++ lib/utils/ed25519.ts
@@ -1,7 +1,12 @@
 import { ed25519 } from "@noble/curves/ed25519";
 import { concat } from "./uint8array.ts";
 
-export function generateKeyPair() {
+export interface KeyPair {
+  secretKey: Uint8Array;
+  publicKey: Uint8Array;
+}
+
+export function generateKeyPair(): KeyPair {
   const privateKey = ed25519.utils.randomPrivateKey();
   const publicKey = ed25519.getPublicKey(privateKey);
   const secretKey = concat(privateKey, publicKey);
@@ -10,8 +15,5 @@ export function generateKeyPair() {
     publicKey,
   };
 }
-
-export const {
-  sign,
-  verify,
-} = ed25519;
+export const sign = ed25519.sign;
+export const verify = ed25519.verify;
